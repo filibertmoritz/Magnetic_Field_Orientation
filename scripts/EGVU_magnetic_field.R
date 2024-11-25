8687b97e0b18762304d3aabfd9d7c47f07ac5a2b
filibertmoritz <filibert.heim@posteo.de>
2024-11-25 12:31:38 +0100

    apply script from REKI on EGVU data

--- scripts/EGVU_magnetic_field.R
+++ scripts/EGVU_magnetic_field.R
@@ -1,5 +1,5 @@
 
-#### Script for studying magnetic orientation of raptors, here REKI 
+#### Script for studying magnetic orientation of raptors, here EGVU 
 #### Script written by Filibert Heim, filibert.heim@posteo.de, following instructions from Steffen Oppel and hints from Will Scheider 
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -10,50 +10,93 @@
 library(oce)
 library(sf)
 library(terra)
+library(readxl)
+library(lubridate)
+library(move2)
 # library(stars) this is another packages that might have advantages for combining raster and sf workflows
 library(tidyverse)
 select <- dplyr::select
 filer <- dplyr::filter 
 rename <- dplyr::rename
 
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 2. Data loading and formatting  --------
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 # read in data 
-data <- read.csv(file = 'data/REKI_sample_locations10.csv') 
+locs <- read.csv(file = 'data/EGVU_sample_locations10.csv', sep = ',', dec = '.', header = T) # exported data from Steffen 
+birds <- readRDS(file = 'data/EGVU_15min_locs.rds') %>% mt_track_data() %>%# other data drom Steffen just to get information on single individuals
+  dplyr::rename(bird_id = individual_local_identifier, comments = individual_comments) %>% 
+  mutate(latest_date_born = if_else(is.na(latest_date_born), exact_date_of_birth, latest_date_born)) %>% 
+  select(individual_id, deployment_id, comments, bird_id, ring_id, sex, latest_date_born, tag_local_identifier) # include certain columns here if more information on the birds is needed 
+mig <- read_excel(path = 'data/Mig months.xlsx', sheet = 'Mig months') 
+str(locs) # check data structure
+str(birds)
 
-# check data structure 
-head(data)
-str(data)
+# improve format for locs
+locs <- locs %>% mutate(timestamp = as_datetime(timestamp), 
+                        bird_name = as.factor(individual_local_identifier), 
+                        tag_id = as.factor(tag_local_identifier), 
+                        study_id = as.factor(study_id), 
+                        ymo = paste0(year(timestamp), '_', format(timestamp, '%m'))) %>% 
+  select(-individual_local_identifier, -tag_local_identifier, -visible)
 
-# improve data structure 
-data <- data %>% 
-  mutate(timestamp = as.POSIXct(timestamp, format = "%Y-%m-%dT%H:%M:%SZ", tz = "UTC"), 
-         migration = as.factor(if_else(MIGRATION == 'migrating', 'migratory', 'stationary')),
-         long = as.numeric(sub("\\|.*", "", geometry)), 
-         lat = as.numeric(sub(".*\\|", "", geometry))) %>% # also as.character
-  select(-MIGRATION, -geometry, -inclination, -declination, -intensity)
+#improve format for birds 
+birds <- birds %>% rename(bird_name = bird_id, tag_id = tag_local_identifier) %>% 
+  select(bird_name, tag_id, sex, latest_date_born)
+
+# improve format of mig
+mig[,c(1,3,17:18)] <- NULL
+mig <- mig[grep(pattern = paste0(birds$tag_id, collapse = '|'), x = mig$file_list),] # this filters for all bird_ids (names of the birds) but doesnt catch all of them because some were born in 2020 and the data is from 2019
+mig <- mig %>%
+  separate(file_list, into = c("bird_name", "tag_id", "year"), sep = "_", extra = "drop") %>%
+  mutate(year = gsub("\\.png$", "", year)) %>% 
+  pivot_longer(cols = c('Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'), names_to = 'month', values_to = 'migration') %>% 
+  mutate(month = case_when(month == 'Jan' ~ '01', month == 'Feb' ~ '02', month == 'Mar' ~ '03', month == 'Apr' ~ '04', month == 'May' ~ '05', month == 'Jun' ~ '06', month == 'Jul' ~ '07', month == 'Aug' ~ '08', month == 'Sep' ~ '09',
+                           month == 'Oct' ~ '10', month == 'Nov' ~ '11', month == 'Dec' ~ '12')) %>% 
+  mutate(ymo = paste0(as.character(year), '_', month)) %>% 
+  mutate(migration = case_when(migration == 0 ~ NA, migration == 1 ~ 'stationary', migration == 2 ~ 'migratory'), 
+         ymo = paste0(year,'_', month)) 
+
+head(mig)
+head(birds, 10)
+head(locs)
+
+# bring locs and bird data together, calculate age_cy and long format
+data <- locs %>% left_join(birds, by = join_by(bird_name, tag_id)) %>% 
+  mutate(age_cy = as.integer(as.integer(timestamp - latest_date_born)/365)) %>% 
+  rename(long = location_long, lat = location_lat) %>% 
+  select(-study_id, -declination, -inclination, -intensity) # remove all unneeded columns 
+
+# add manually annotated migration data 
+data <- data %>% left_join(mig %>% select(-tag_id, -Comments), by = join_by(bird_name, ymo)) %>% 
+  mutate(migration = factor(migration))
+data <- data %>% drop_na(migration) # remove all rows for which no information on migration exists (this is because the classification has been done manually in 2019 but we also use data from afterwards)
 
 # create column which tells whether the current migration was the first autumn migration 
 data <- data %>%
-  group_by(id) %>%
+  group_by(bird_name) %>%
   mutate(
     first_migration = cumsum(migration == 'migratory' & lag(migration, default = 'stationary') != 'migratory'),
     first_migration = ifelse(migration == 'migratory', first_migration, NA)) %>%
   ungroup() 
 
 data <- data %>% 
-  group_by(id) %>% 
+  group_by(bird_name) %>% 
   filter(migration == 'migratory') %>% 
   mutate(
     first_migration = if_else(year(timestamp) == year(min(timestamp, na.rm = TRUE)), 1, first_migration), 
     first_migration = if_else(year(timestamp) == year(min(timestamp, na.rm = TRUE)) + 1 & timestamp < as.POSIXct(paste0(year(min(timestamp, na.rm = TRUE)) + 1, '-07-15 00:00:00'), format = "%Y-%m-%d %H:%M:%S"), 2, first_migration)) %>%
-  filter(first_migration %in% c(1,2)) %>% select(id, timestamp, first_migration) %>% 
-  right_join(data %>% select(-first_migration), by = join_by(id, timestamp))
+  filter(first_migration %in% c(1,2)) %>% select(bird_name, timestamp, first_migration) %>% 
+  right_join(data %>% select(-first_migration), by = join_by(bird_name, timestamp))
 
 data <- data %>% mutate(first_migration = factor(case_when(first_migration == 1 ~ 'autumn', 
                                              first_migration == 2 ~ 'spring', 
                                             TRUE ~ as.character(first_migration))))
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# 2. Create a raster for the migration route of REKI's  --------
+# 3. Create a raster for the migration route of EGVU's  --------
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 # create binding box 
@@ -63,32 +106,32 @@ bbox <- terra::ext(range(data$long), range(data$lat))
 rast <- rast(ext = bbox, resolution = 0.5, crs = "EPSG:4258") # not sure if maybe the world EPSG would be better, just replace by 'EPSG:4326'
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# 3. Get magnetic field values for first spring migration in raster  --------
+# 4. Get magnetic field values for first spring migration in raster  --------
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 # calculate mean autumn migration date a
 data <- data %>%
-  group_by(id) %>%
+  group_by(bird_name) %>%
   mutate(mean_spring = if_else(first_migration == 'spring', 
                                mean(timestamp[first_migration == 'spring'], na.rm = TRUE), 
                                as.POSIXct(NA)))
 
 # create df to extract magnetic field values for autumn migration for every bird 
-
-birds <- unique(data$id) # get ids from all birds 
-mean_spring <- data %>% filter(first_migration == 'spring') %>% group_by(id) %>% summarise(mean_spring = mean(timestamp)) # get mean autumn migration dates 
+birds <- unique(data$bird_name) # get ids from all birds 
+mean_spring <- data %>% filter(first_migration == 'spring') %>% group_by(bird_name) %>% summarise(mean_spring = mean(timestamp)) # get mean autumn migration dates 
 
 df_rast <- as.data.frame(xyFromCell(rast, 1:ncell(rast))) # get the coordinated from raster
 df_rast <- df_rast %>% rename(long = x, lat = y) # rename raster
 
 df_rast <- data.frame(long = rep(df_rast$long, times = length(birds)), 
                       lat = rep(df_rast$lat, times = length(birds)), 
-                      id = rep(birds, each = nrow(df_rast)),
+                      bird_name = rep(birds, each = nrow(df_rast)),
                       inclination = NA, 
                       declination = NA, 
                       intensity = NA)
 
-df_rast <- df_rast %>% left_join(mean_spring, by = join_by(id)) # add the mean_spring migration dates 
+df_rast <- df_rast %>% left_join(mean_spring, by = join_by(bird_name)) # add the mean_spring migration dates 
+df_rast <- df_rast %>% drop_na(mean_spring) # get rid of all cases where no spring migration date could be calculates because of missing data 
 
 # get magnetic field values for mean date of each birds first spring migration
 df_rast$inclination <- magneticField(longitude = df_rast$long, latitude = df_rast$lat, time = df_rast$mean_spring)$inclination
@@ -96,7 +139,7 @@ df_rast$declination <- magneticField(longitude = df_rast$long, latitude = df_ras
 df_rast$intensity <- magneticField(longitude = df_rast$long, latitude = df_rast$lat, time = df_rast$mean_spring)$intensity
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# 3. Get magnetic field values for first autumn migration for all locations of each bird --------
+# 4. Get magnetic field values for first autumn migration for all locations of each bird --------
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 # filter for autumn migration
@@ -109,7 +152,7 @@ data_autumn$inclination <- magneticField(longitude = data_autumn$long, latitude 
 data_autumn$intensity <- magneticField(longitude = data_autumn$long, latitude = data_autumn$lat, time = data_autumn$timestamp)$intensity
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# 4. Get information of grid cells where autumn migration values lied within spring migration  --------
+# 5. Get information of grid cells where autumn migration values lied within spring migration  --------
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 # round values which represents birds ability to sense magnetic field values, ATTENTION: only autumn migration magnetic field values are rounded, not spring migration
@@ -123,8 +166,8 @@ data_autumn$intensity_max <- data_autumn$intensity + 200 # intensity sensitivity
 data_autumn$intensity_min <- data_autumn$intensity - 200
 
 # create one df of data_autumn and df_rast from spring migration for comparison
-overlap <- df_rast %>% left_join(data_autumn %>% select(-long, -lat, -mean_spring), by = join_by(id), suffix = c('_spring', '_autumn')) %>% 
-  select(-migration, -first_migration, -distance_to_next, -speed) %>% 
+overlap <- df_rast %>% left_join(data_autumn %>% select(-long, -lat, -mean_spring), by = join_by(bird_name), suffix = c('_spring', '_autumn')) %>% 
+  select(-migration, -first_migration) %>% 
   mutate(match_inclination = inclination_spring >= inclination_min & inclination_spring <= inclination_max, # create a column that checks whether inclination from spring lies between max and min of autumn migration
          match_declination = declination_spring >= declination_min & declination_spring <= declination_max,
          match_intensity = intensity_spring >= intensity_min & intensity_spring <= intensity_max, 
@@ -133,7 +176,7 @@ overlap <- df_rast %>% left_join(data_autumn %>% select(-long, -lat, -mean_sprin
   filter(match_all == TRUE) # romve all grid cells that are not needed
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# 5. Plot data --------
+# 6. Plot data --------
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 # create a plot 
@@ -143,16 +186,16 @@ ggplot() +
   geom_path(data = data %>% filter(first_migration == 'spring'), 
             mapping = aes(x = long, y = lat, color = 'Spring Migration')) +
   coord_equal() + 
-  facet_wrap(~id) +
-  labs(title = "Experienced Magnetic Field During Autumn Migraion and Spring Migration route for ten Red Kites",
+  facet_wrap(~bird_name) +
+  labs(title = "Experienced Magnetic Field During Autumn Migraion and Spring Migration route for ten Egyptian Vulture",
     x = "Longitude",
     y = "Latitude") +
   scale_color_manual(name = 'Legend', values = c('Experienced Magentic Field in Autumn' = 'grey80', 'Spring Migration' = 'red')) +
   theme_bw() + 
-  theme(legend.position = c(0.75,0.12), 
+  theme(legend.position = c(0.85,0.2), 
         legend.title = element_text(size = 14),
         legend.text = element_text(size = 12), 
         plot.title = element_text(size = 15), 
         axis.title = element_text(size = 12))
-ggsave(filename = 'output/REKI_magnetic_field_autumn_spring.png', height = 9, width = 12)
+ggsave(filename = 'output/EGVU_magnetic_field_autumn_spring.png', height = 9, width = 12)
 
